85ee300a9ec87b4c5e70b2af1f4aa22b1ad1792a
Pedro Feld <pedro.gael.feld@gmail.com>
2026-01-19 20:57:22 -0300

    refactor: problema no prisma client, o módulo não está sendo instalado

--- src/database/repository.ts
+++ src/database/repository.ts
@@ -1,5 +1,8 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client')
 
 const repository = new PrismaClient();
 
 export default repository;
+
+// problema no prisma client
+// foi necessário usar o require ao invés do import
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
+import { UserService } from "./services/user.service"
+
 console.log("Hello, world!")
+
+const userService = new UserService();
+
+userService.criar({
+    nome: "Pedro",
+    email: "pedro.gael.feld@gmail.com",
+    senha: 123456,
+    endereco: {
+        cidade: "Sapiranga",
+        uf: "RS",
+        rua: "Rua A",
+        numero: 100,
+    },
+    dtNascimento: new Date("2004-08-17"),
+})
+.then(result => console.log(result))
+.catch(error => console.log(error));
+
+// não está sendo possível criar um novo user devido a um problema com o prisma client
+// npm run dev retorna com erro, ele não localiza o módulo '@prisma/client'
